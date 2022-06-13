0a895a3bb2628831f650364eb53af1b06776d39d
couriourc <3234853521@qq.com>
2022-06-13 20:05:04 +0800

    fix: return button tag

--- src/components/returnButton.vue
+++ src/components/returnButton.vue
@@ -6,7 +6,7 @@
       class="return-button bg-light-50 border-none outline-none p-2 rounded-bl-lg rounded-br-lg hover:bg-green hover:text-white hover:shadow-lg duration-700"
       @click="$router.replace('/')"
     >
-      <i class="i-icon-carbon-github">return</i>
+      <span class="i-icon-carbon-github">return</span>
     </button>
     <div class="w-100% text-center text-white cursor-pointer">{{
       $attrs['page-title'] || $route.name
